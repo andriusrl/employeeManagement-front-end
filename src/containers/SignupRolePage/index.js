7a5728fd4f8f14ba2985ddaa03188ff968b9ad8a
Andrius <andrius.rochalazarino@gmail.com>
2020-09-11 10:18:28 -0400

    Adicionado caixa de dialogo quando se cadastra cargo

--- src/containers/SignupRolePage/index.js
+++ src/containers/SignupRolePage/index.js
@@ -6,26 +6,38 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { addRole } from "../../actions/role";
-
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
 
 const SignupRolePageWrapper = styled.form`
     display: flex;
     flex-direction: column;
     align-items: center;
 `
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 class SignupRolePage extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             inputRole: "",
+            dialogStatus: false
         }
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
         this.props.addRole(this.state.inputRole)
-        alert(`Cargo [${this.state.inputRole}] cadastrado com sucesso!`)
+        this.handleClickOpen()
+        this.setState({
+            inputRole: ""
+        })
     }
 
     handleInputChange = (e) => {
@@ -34,10 +46,20 @@ class SignupRolePage extends React.Component {
         })
     }
 
+    handleClickOpen = () => {
+        this.setState({
+            dialogStatus: true
+        })
+    };
     
+    handleClose = () => {
+        this.setState({
+            dialogStatus: false
+        })
+    };
+
+
     render() {
-        console.log("Testando valor redux role")
-        console.log(this.props.getRole)
         return (
             <SignupRolePageWrapper onSubmit={this.handleSubmit}>
                 <Typography variant="h5" gutterBottom>
@@ -50,11 +72,31 @@ class SignupRolePage extends React.Component {
                     type="submit"
                     variant="contained"
                     color="primary"
-                    // endIcon={<Icon>send</Icon>}
+                // endIcon={<Icon>send</Icon>}
                 >
                     Cadastrar
                 </Button>
                 <Box m={1} />
+                <Dialog
+                    open={this.state.dialogStatus}
+                    TransitionComponent={Transition}
+                    keepMounted
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-slide-title"
+                    aria-describedby="alert-dialog-slide-description"
+                >
+                    <DialogTitle>{"Cargo criado com sucesso!"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Nome do cargo: {this.props.getRole[this.props.getRole.length-1]}
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Fechar
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </SignupRolePageWrapper>
         )
     }
@@ -64,13 +106,10 @@ const mapStateToProps = state => {
     return {
         getRole: state.role
     }
-  }
+}
 
 const mapDispatchToProps = dispatch => ({
     addRole: (role) => dispatch(addRole(role))
-  })
-  
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignupRolePage);
-
-
